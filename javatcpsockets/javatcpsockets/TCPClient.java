ec62d7031e8ce95f605ca5fc8f1a6e004d3ad914
vfloresp <vflores@pop-os.localdomain>
2021-01-27 18:58:09 -0600

    conexion tcp

---
+++ javatcpsockets/javatcpsockets/TCPClient.java
@@ -0,0 +1,39 @@
+
+package javatcpsockets;
+
+import java.net.*;
+import java.io.*;
+
+public class TCPClient {
+
+    public static void main(String args[]) {
+
+        Socket s = null;
+        try {
+            int serverPort = 7896;
+
+            s = new Socket("localhost", serverPort);
+            //   s = new Socket("127.0.0.1", serverPort);
+            DataInputStream in = new DataInputStream(s.getInputStream());
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            out.writeUTF("Hello");            // UTF is a string encoding
+            out.writeInt(1);
+
+            String data = in.readUTF();
+            System.out.println("Received: " + data);
+        } catch (UnknownHostException e) {
+            System.out.println("Sock:" + e.getMessage());
+        } catch (EOFException e) {
+            System.out.println("EOF:" + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        } finally {
+            if (s != null)
+                try {
+                    s.close();
+                } catch (IOException e) {
+                    System.out.println("close:" + e.getMessage());
+                }
+        }
+    }
+}
---
+++ javatcpsockets/javatcpsockets/TCPServer.java
@@ -0,0 +1,63 @@
+
+package javatcpsockets;
+
+import java.net.*;
+import java.io.*;
+
+
+public class TCPServer {
+
+    public static void main(String args[]) {
+        try {
+            int serverPort = 7896;
+            ServerSocket listenSocket = new ServerSocket(serverPort);
+            while (true) {
+                System.out.println("Waiting for messages...");
+                Socket clientSocket = listenSocket.accept();  // Listens for a connection to be made to this socket and accepts it. The method blocks until a connection is made.
+                Connection c = new Connection(clientSocket);
+                c.start();
+            }
+        } catch (IOException e) {
+            System.out.println("Listen :" + e.getMessage());
+        }
+    }
+
+}
+
+class Connection extends Thread {
+    DataInputStream in;
+    DataOutputStream out;
+    Socket clientSocket;
+
+    public Connection(Socket aClientSocket) {
+        try {
+            clientSocket = aClientSocket;
+            in = new DataInputStream(clientSocket.getInputStream());
+            out = new DataOutputStream(clientSocket.getOutputStream());
+        } catch (IOException e) {
+            System.out.println("Connection:" + e.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+        try {                             // an echo server
+            String data = in.readUTF();
+            int idUsuario = in.readInt();
+            System.out.println("Message received from: " + clientSocket.getRemoteSocketAddress());
+            out.writeUTF(data);
+        } catch (EOFException e) {
+            System.out.println("EOF:" + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+
